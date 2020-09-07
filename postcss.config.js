2c0dc07f2c4c5a31fa42f54e0174cdb5eb2f13e1
Franco <chenhuifan.gg@gmail.com>
2020-09-07 17:04:48 +0800

    Main YMUI theme variables file configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 const autoprefixer = require("autoprefixer")();
+const postcssnested = require("postcss-nested")();
 const tailwindcss = require("tailwindcss")("./tailwind.config.js");
+const postcsscustomproperties = require("postcss-custom-properties")();
 
 module.exports = {
-  plugins: [tailwindcss, autoprefixer]
+  plugins: [tailwindcss, postcssnested, postcsscustomproperties, autoprefixer]
 };
